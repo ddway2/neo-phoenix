c803eda211c6d2f6597914dcd734753582c9555b
Pierre Salmon <pierre.salmon@expandium.com>
2016-07-25 16:17:39 +0200

    Add sample to use onbuild docker image

---
+++ samples/app/sources/binaries/hello_world/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
+
+#if 0
+=pod
+
+=head1 NAME
+
+copernic_broker - an expandium tool
+
+=head1 DESCRIPTION
+
+Generate an euuid.
+
+=head1 SYNOPSIS
+
+  prog
+
+=cut
+#endif

bcc80bced9ecfa32879739f84a8be6857b4561a9
Pierre Salmon <pierre.salmon@expandium.com>
2016-07-25 09:30:35 +0200

    Used debian:stretch base - Add buidl script

---
+++ builder/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_DIR=$1
+[ -z "$PROJECT_DIR" ] && PROJECT_DIR=/src/build
+
+cd $PROJECT_DIR
+echo "Directory to build $PROJECT_DIR"
+[ -f Makefile ] && make distclean
+
+cbuild -C -c -y
+make install
+
